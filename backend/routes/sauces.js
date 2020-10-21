fa53ddc5e92dc48ab46ce772fb358ef423c28a42
bmtestoc <60735933+bmtestoc@users.noreply.github.com>
2020-10-21 20:58:16 +0200

    Add files via upload

--- backend/routes/sauces.js
+++ backend/routes/sauces.js
@@ -14,8 +14,6 @@ router.post('/', auth, multer, saucesCtrl.createSauce);
 router.get('/:id', auth, saucesCtrl.getOneSauce);
 router.put('/:id', auth, multer, saucesCtrl.modifySauce);
 router.delete('/:id', auth, saucesCtrl.deleteSauce);
-router.post("/:id/like", saucesCtrl.likeSauce);
+router.post("/:id/like", auth, saucesCtrl.likeSauce);
 
 module.exports = router;
-
-// !!!!!!!!!!!!!!!!! router.post("/:id/like", auth, saucesCtrl.likeSauce);
