e334188827890ee01fdeae0a6c398093204b551f
bmtestoc <60735933+bmtestoc@users.noreply.github.com>
2020-10-28 18:51:13 +0100

    Add files via upload

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const userCtrl = require('../controllers/user');
+const bouncer = require('express-bouncer')(5000, 10000, 2);
 
 // Routes de l'API
 router.post('/signup', userCtrl.signup);
-router.post('/login', userCtrl.login);
+router.post('/login', bouncer.block, userCtrl.login);
 
 module.exports = router;
