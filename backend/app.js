4468feaed7da4adff83763a77686914960d8d979
bmtestoc <60735933+bmtestoc@users.noreply.github.com>
2020-11-03 20:11:38 +0100

    Add files via upload

--- backend/app.js
+++ backend/app.js
@@ -17,18 +17,20 @@ app.use(cors({
 }));
 
 // Connexion à MongoDB
-mongoose.connect('mongodb+srv://bm:bm123@sopekocko.hx6se.mongodb.net/p6ocr?retryWrites=true&w=majority',
-  { useNewUrlParser: true,
-    useUnifiedTopology: true })
+mongoose.connect('mongodb+srv://login:mdp@sopekocko.hx6se.mongodb.net/p6ocr?retryWrites=true&w=majority',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
   .then(() => console.log('Connexion à MongoDB réussie !'))
   .catch(() => console.log('Connexion à MongoDB échouée !'));
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-    next();
-  });
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
+});
 
 // Pour pouvoir utiliser les données du corps de la requête
 app.use(bodyParser.json());
--- backend/models/sauces.js
+++ backend/models/sauces.js
@@ -2,17 +2,17 @@ const mongoose = require('mongoose');
 
 // Modèle sauces
 const saucesSchema = mongoose.Schema({
-    userId: {type: String, required: true},
-    name: {type: String, required: true},
-    manufacturer: {type: String, required: true},
-    description: {type: String, required: true},
-    mainPepper: {type: String, required: true},
-    imageUrl: {type: String, required: true},
-    heat: {type: Number, required: true},
-    likes: {type: Number, required: false, default:0},
-    dislikes: {type: Number, required: false, default:0},
-    usersLiked: {type: [String], required: false},
-    usersDisliked: {type: [String], required: false},
+    userId: { type: String, required: true },
+    name: { type: String, required: true },
+    manufacturer: { type: String, required: true },
+    description: { type: String, required: true },
+    mainPepper: { type: String, required: true },
+    imageUrl: { type: String, required: true },
+    heat: { type: Number, required: true },
+    likes: { type: Number, required: false, default: 0 },
+    dislikes: { type: Number, required: false, default: 0 },
+    usersLiked: { type: [String], required: false },
+    usersDisliked: { type: [String], required: false },
 });
 
 module.exports = mongoose.model('Sauces', saucesSchema);
